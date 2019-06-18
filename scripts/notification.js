784afb4ead0ad137b226f524344b46f2e48c535a
Isabella Wang <wangxbella0108@gmail.com>
2019-06-18 11:47:33 -0400

    update google map api

--- scripts/notification.js
+++ scripts/notification.js
@@ -1,3 +1,5 @@
+// install npm install node-schedule
+
 const Nexmo = require('nexmo')
 
 const nexmo = new Nexmo({
@@ -5,11 +7,44 @@ const nexmo = new Nexmo({
   apiSecret: "O7gIZrtj1GsltaeL"
 })
 
+// fetch data from the google calendar
+var origin=where;
+var destination=where;
+// put time in the following format 
+// replace the result. ex yyyy=1997, MM=12.
+var year=yyyy;
+var month=MM;
+var day=dd;
+var hour=hh;
+var minuteInCalendar=mm;
+var inputTime=5;
+// add 2 more minutes to their preferred TimeRanges, 
+// becuase people need time to react when they see the message
+var perferredTimeBefore=inputTime+5;
+var minute=0;
 
-const from = '13852403539'
-const to = phoneNumber
-const text = 'Hello, this is Dear Time. Thanks for subscribing us. We will help you to get to your place on time '
+// calculate the execution time
+if((minuteInCalendar-perferredTimeBefore-travalTime)<0){
+    minute=60+(minuteInCalendar-perferredTimeBefore);
+}else if((minuteInCalendar-perferredTimeBefore-travalTime)>=0){
+    minute=minuteInCalendar-perferredTimeBefor;
+}
+// get the actual time that we will execute the time
+var date = new Date(year, month, day, hour, minute,0);
 
+var linkInText="https://www.google.com/maps/dir/?api=1&origin="+origin+"&destination="+destination;
+
+// this is our default virtual number
+const from = '13852403539';
+// set message receiver
+var to = '19083916750';
+// make the text dynamic
+const text = 'Hello, this is Dear Time. Thanks for subscribing us. We will help you to get to your place on time. Click here to see the route: '+linkInText;
+
+
+// send text
+function sendMessage(){ 
+    
 nexmo.message.sendSms(from, to, text, (err, responseData) => {
     if (err) {
         console.log(err);
@@ -20,4 +55,27 @@ nexmo.message.sendSms(from, to, text, (err, responseData) => {
             console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
         }
     }
+})
+
+ };
+
+
+// route calculation
+$.ajax({
+    url:"https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+destination+"&key=AIzaSyBN7LY1mgo8xNyiqviHoMbxTnrT4whKLXM",
+    method:"GET"
+}).then(function(response){
+    
+    var travalTime=response.routes[0].legs[0].duration.text;
+
+
+// Node.js schedule time to run function
+    var schedule = require('node-schedule');
+ 
+     schedule.scheduleJob(date, function(){
+        
+        //  execute the sendMessage function when it hist time
+        sendMessage();
+        });
+
 })
